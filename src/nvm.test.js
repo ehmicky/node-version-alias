e3a1276b4ab0a0f03a7f68739979f137bdb17c45
ehmicky <ehmicky@gmail.com>
2022-12-04 23:23:16 +0100

    Move files

--- src/nvm.test.js
+++ src/nvm.test.js
@@ -1,21 +1,64 @@
+import { execFile } from 'node:child_process'
+import { createWriteStream } from 'node:fs'
+import { readFile, writeFile, unlink } from 'node:fs/promises'
+import { join, dirname } from 'node:path'
 import { env, version as processVersion } from 'node:process'
+import { pipeline } from 'node:stream'
+import { fileURLToPath } from 'node:url'
+import { promisify } from 'node:util'
 
 import test from 'ava'
+import got from 'got'
 import nodeVersionAlias from 'node-version-alias'
 
-import {
-  NVM_DIR,
-  downloadNvm,
-  cleanupNvm,
-  runNvmCommand,
-} from './helpers/nvm/main.test.js'
+// eslint-disable-next-line import/max-dependencies
 import { FULL_VERSION, UNKNOWN_ALIAS } from './helpers/versions.test.js'
+
+const pExecFile = promisify(execFile)
+// TODO: use `stream/promises` instead once dropping support for Node <15.0.0
+const pPipeline = promisify(pipeline)
+
+const NVM_URL = 'https://raw.githubusercontent.com/nvm-sh/nvm/master/nvm.sh'
+const NVM_DIR = dirname(fileURLToPath(import.meta.url))
+const NVM_DIST = join(NVM_DIR, 'nvm.sh')
 
 // eslint-disable-next-line fp/no-mutation
 env.NVM_DIR = NVM_DIR
 
+// We test `nvm` by downloading it locally. It is a Bash script and is not on
+// `npm` so we need to download it.
+const downloadNvm = async function () {
+  const response = await got.stream(NVM_URL)
+  const stream = createWriteStream(NVM_DIST)
+  await pPipeline(response, stream)
+
+  await commentLine()
+}
+
+// `nvm.sh` last line executes `nvm use`. We need to comment it for tests.
+const commentLine = async function () {
+  const content = await readFile(NVM_DIST, 'utf8')
+  const contentA = content.replace(COMMENTED_LINE, '# $&')
+  await writeFile(NVM_DIST, contentA)
+}
+
+const COMMENTED_LINE = 'nvm_process_parameters "$@"'
+
+const cleanupNvm = async function () {
+  await unlink(NVM_DIST)
+}
+
 test.before(downloadNvm)
 test.after(cleanupNvm)
+
+// Run `nvm` command in tests
+const runNvmCommand = async function (command) {
+  const { stdout } = await pExecFile('bash', [
+    '-c',
+    `source "${NVM_DIST}" && ${command}`,
+  ])
+  return stdout.trim()
+}
 
 test('Can use "system" alias', async (t) => {
   const version = await nodeVersionAlias('system')
